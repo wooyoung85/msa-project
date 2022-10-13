c5f32c04067417e064d69c9705bec5a272c050db
sg-wooyoung <wyseo@seegene.com>
2022-10-13 16:11:37 +0900

    initial commit

---
+++ discovery/src/test/java/com/woodong/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woodong.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ user/src/test/java/com/woodong/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woodong.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
