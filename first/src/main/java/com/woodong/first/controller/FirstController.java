6add8bfb91d412673d3ac00464d58380cf261bc6
sg-wooyoung <wyseo@seegene.com>
2022-10-14 13:54:29 +0900

    spring cloud gateway filter 적용

--- first/src/main/java/com/woodong/first/controller/FirstController.java
+++ first/src/main/java/com/woodong/first/controller/FirstController.java
@@ -1,6 +1,7 @@
-package com.woodong.first;
+package com.woodong.first.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,11 @@ public class FirstController {
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to the First Service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header){
+        System.out.println(header);
+        return "Hello World in First Service";
     }
 }
--- second/src/main/java/com/woodong/second/controller/SecondController.java
+++ second/src/main/java/com/woodong/second/controller/SecondController.java
@@ -1,6 +1,7 @@
-package com.woodong.second;
+package com.woodong.second.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,11 @@ public class SecondController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the Second Service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header){
+        System.out.println(header);
+        return "Hello World in Second Service";
     }
 }
