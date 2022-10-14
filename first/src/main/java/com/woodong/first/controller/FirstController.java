f7a42aaa9db6e4c19b3bcb1c1b379afb001828c8
sg-wooyoung <wyseo@seegene.com>
2022-10-14 15:55:28 +0900

    spring cloud gateway - eureka 연동, load balancing 테스트

--- first/src/main/java/com/woodong/first/controller/FirstController.java
+++ first/src/main/java/com/woodong/first/controller/FirstController.java
@@ -1,13 +1,21 @@
 package com.woodong.first.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service")
+@Slf4j
 public class FirstController {
+
+    @Value("${server.port}")
+    private String serverPort;
 
     @GetMapping("/welcome")
     public String welcome(){
@@ -18,5 +26,11 @@ public class FirstController {
     public String message(@RequestHeader(value = "first-request", required = false) String header){
         System.out.println(header);
         return "Hello World in First Service";
+    }
+
+    @GetMapping("/check")
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        return String.format("Hi, there. This is a message from First Service on Port %s.", serverPort);
     }
 }
